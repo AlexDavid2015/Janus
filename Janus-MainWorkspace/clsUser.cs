a9c839fe9d1aaa6d09f90939856a6ae235bfccb8
YaoJin <yaojingmasnus@gmail.com>
2015-01-21 14:43:28 +0800

    add clsUser member variables

--- Janus-MainWorkspace/clsUser.cs
+++ Janus-MainWorkspace/clsUser.cs
@@ -8,5 +8,45 @@ namespace CxTitan
 {
     public class clsUser
     {
+        private int iD;
+        private string userName;
+        private string passWord;
+        private short level;
+
+        public clsUser()
+        {
+            InitializeUser();
+        }
+
+        public void InitializeUser()
+        {
+            iD = 0;
+            userName = "No user logged in..";
+            level = 0;
+        }
+
+        public int ID
+        {
+            get { return iD; }
+            set { iD = value; }
+        }
+
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+
+        public string PassWord
+        {
+            get { return passWord; }
+            set { passWord = value; }
+        }
+
+        public short Level
+        {
+            get { return level; }
+            set { level = value; }
+        }
     }
 }
