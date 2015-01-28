d40e75eb00cca0a74db590f1d6aa0771a4722194
YaoJin <yaojingmasnus@gmail.com>
2015-01-28 18:05:04 +0800

    add button and other UI items in the Programs Page

--- Janus-MainWorkspace/Programs.Designer.cs
+++ Janus-MainWorkspace/Programs.Designer.cs
@@ -29,24 +29,178 @@
         private void InitializeComponent()
         {
             this.cmdExit = new System.Windows.Forms.Button();
+            this.gpbProgramPanel = new System.Windows.Forms.GroupBox();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.lstFind = new System.Windows.Forms.ListBox();
+            this.cmdFind = new System.Windows.Forms.Button();
+            this.cmdExport = new System.Windows.Forms.Button();
+            this.cmdImport = new System.Windows.Forms.Button();
+            this.cmdNew = new System.Windows.Forms.Button();
+            this.cmdModify = new System.Windows.Forms.Button();
+            this.cmdDelete = new System.Windows.Forms.Button();
+            this.cmdDownload = new System.Windows.Forms.Button();
+            this.cmdPairingProgramProduct = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // cmdExit
             // 
-            this.cmdExit.Location = new System.Drawing.Point(745, 516);
+            this.cmdExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.cmdExit.Location = new System.Drawing.Point(668, 594);
             this.cmdExit.Name = "cmdExit";
-            this.cmdExit.Size = new System.Drawing.Size(75, 75);
+            this.cmdExit.Size = new System.Drawing.Size(212, 61);
             this.cmdExit.TabIndex = 6;
             this.cmdExit.Text = "Exit";
             this.cmdExit.UseVisualStyleBackColor = true;
             this.cmdExit.Click += new System.EventHandler(this.cmdExit_Click);
             // 
+            // gpbProgramPanel
+            // 
+            this.gpbProgramPanel.Location = new System.Drawing.Point(22, 12);
+            this.gpbProgramPanel.Name = "gpbProgramPanel";
+            this.gpbProgramPanel.Size = new System.Drawing.Size(603, 429);
+            this.gpbProgramPanel.TabIndex = 74;
+            this.gpbProgramPanel.TabStop = false;
+            this.gpbProgramPanel.Text = "Program Panel";
+            // 
+            // txtFind
+            // 
+            this.txtFind.Location = new System.Drawing.Point(668, 12);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(212, 20);
+            this.txtFind.TabIndex = 75;
+            this.txtFind.Text = "*";
+            // 
+            // lstFind
+            // 
+            this.lstFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstFind.FormattingEnabled = true;
+            this.lstFind.ItemHeight = 24;
+            this.lstFind.Location = new System.Drawing.Point(668, 73);
+            this.lstFind.Name = "lstFind";
+            this.lstFind.Size = new System.Drawing.Size(212, 364);
+            this.lstFind.TabIndex = 76;
+            // 
+            // cmdFind
+            // 
+            this.cmdFind.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.cmdFind.FlatAppearance.BorderSize = 0;
+            this.cmdFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdFind.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.cmdFind.Location = new System.Drawing.Point(668, 460);
+            this.cmdFind.Name = "cmdFind";
+            this.cmdFind.Size = new System.Drawing.Size(212, 61);
+            this.cmdFind.TabIndex = 77;
+            this.cmdFind.Text = "Find";
+            this.cmdFind.UseVisualStyleBackColor = true;
+            // 
+            // cmdExport
+            // 
+            this.cmdExport.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.cmdExport.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.cmdExport.FlatAppearance.BorderSize = 0;
+            this.cmdExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdExport.ForeColor = System.Drawing.Color.Black;
+            this.cmdExport.Location = new System.Drawing.Point(668, 527);
+            this.cmdExport.Name = "cmdExport";
+            this.cmdExport.Size = new System.Drawing.Size(100, 61);
+            this.cmdExport.TabIndex = 78;
+            this.cmdExport.Text = "Export";
+            this.cmdExport.UseVisualStyleBackColor = false;
+            // 
+            // cmdImport
+            // 
+            this.cmdImport.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.cmdImport.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.cmdImport.FlatAppearance.BorderSize = 0;
+            this.cmdImport.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdImport.ForeColor = System.Drawing.Color.Black;
+            this.cmdImport.Location = new System.Drawing.Point(780, 527);
+            this.cmdImport.Name = "cmdImport";
+            this.cmdImport.Size = new System.Drawing.Size(100, 61);
+            this.cmdImport.TabIndex = 79;
+            this.cmdImport.Text = "Import";
+            this.cmdImport.UseVisualStyleBackColor = false;
+            // 
+            // cmdNew
+            // 
+            this.cmdNew.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.cmdNew.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.cmdNew.FlatAppearance.BorderSize = 0;
+            this.cmdNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdNew.ForeColor = System.Drawing.Color.Black;
+            this.cmdNew.Location = new System.Drawing.Point(22, 460);
+            this.cmdNew.Name = "cmdNew";
+            this.cmdNew.Size = new System.Drawing.Size(265, 61);
+            this.cmdNew.TabIndex = 80;
+            this.cmdNew.Text = "New";
+            this.cmdNew.UseVisualStyleBackColor = false;
+            // 
+            // cmdModify
+            // 
+            this.cmdModify.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.cmdModify.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.cmdModify.FlatAppearance.BorderSize = 0;
+            this.cmdModify.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdModify.ForeColor = System.Drawing.Color.Black;
+            this.cmdModify.Location = new System.Drawing.Point(22, 527);
+            this.cmdModify.Name = "cmdModify";
+            this.cmdModify.Size = new System.Drawing.Size(120, 61);
+            this.cmdModify.TabIndex = 81;
+            this.cmdModify.Text = "Modify";
+            this.cmdModify.UseVisualStyleBackColor = false;
+            // 
+            // cmdDelete
+            // 
+            this.cmdDelete.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.cmdDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.cmdDelete.FlatAppearance.BorderSize = 0;
+            this.cmdDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdDelete.ForeColor = System.Drawing.Color.Black;
+            this.cmdDelete.Location = new System.Drawing.Point(167, 527);
+            this.cmdDelete.Name = "cmdDelete";
+            this.cmdDelete.Size = new System.Drawing.Size(120, 61);
+            this.cmdDelete.TabIndex = 82;
+            this.cmdDelete.Text = "Delete";
+            this.cmdDelete.UseVisualStyleBackColor = false;
+            // 
+            // cmdDownload
+            // 
+            this.cmdDownload.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.cmdDownload.Location = new System.Drawing.Point(453, 460);
+            this.cmdDownload.Name = "cmdDownload";
+            this.cmdDownload.Size = new System.Drawing.Size(172, 128);
+            this.cmdDownload.TabIndex = 83;
+            this.cmdDownload.Text = "Download";
+            this.cmdDownload.UseVisualStyleBackColor = true;
+            // 
+            // cmdPairingProgramProduct
+            // 
+            this.cmdPairingProgramProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.cmdPairingProgramProduct.Location = new System.Drawing.Point(22, 594);
+            this.cmdPairingProgramProduct.Name = "cmdPairingProgramProduct";
+            this.cmdPairingProgramProduct.Size = new System.Drawing.Size(265, 61);
+            this.cmdPairingProgramProduct.TabIndex = 84;
+            this.cmdPairingProgramProduct.Text = "                 PAIRING                   Program && Product";
+            this.cmdPairingProgramProduct.UseVisualStyleBackColor = true;
+            // 
             // Programs
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(864, 621);
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(892, 667);
             this.ControlBox = false;
+            this.Controls.Add(this.cmdPairingProgramProduct);
+            this.Controls.Add(this.cmdDownload);
+            this.Controls.Add(this.cmdDelete);
+            this.Controls.Add(this.cmdModify);
+            this.Controls.Add(this.cmdNew);
+            this.Controls.Add(this.cmdImport);
+            this.Controls.Add(this.cmdExport);
+            this.Controls.Add(this.cmdFind);
+            this.Controls.Add(this.lstFind);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.gpbProgramPanel);
             this.Controls.Add(this.cmdExit);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -54,11 +208,23 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Programs";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         internal System.Windows.Forms.Button cmdExit;
+        internal System.Windows.Forms.GroupBox gpbProgramPanel;
+        internal System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.ListBox lstFind;
+        internal System.Windows.Forms.Button cmdFind;
+        internal System.Windows.Forms.Button cmdExport;
+        internal System.Windows.Forms.Button cmdImport;
+        internal System.Windows.Forms.Button cmdNew;
+        internal System.Windows.Forms.Button cmdModify;
+        internal System.Windows.Forms.Button cmdDelete;
+        internal System.Windows.Forms.Button cmdDownload;
+        internal System.Windows.Forms.Button cmdPairingProgramProduct;
     }
 }
